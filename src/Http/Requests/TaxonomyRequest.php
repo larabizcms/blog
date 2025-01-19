0743f48c306eae8453d8395e3b7ea187cdfd5b0b
Thế Anh Đặng <dangtheanh16@gmail.com>
2025-01-19 15:10:15 +0700

    feat: Remove unused code (master)
    Removed unused docblocks and authorize method from
    TaxonomyRequest.

--- src/Http/Requests/TaxonomyRequest.php
+++ src/Http/Requests/TaxonomyRequest.php
@@ -13,26 +13,11 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class TaxonomyRequest extends FormRequest
 {
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
 			'type' => ['required'],
-			'parent_id' => ['required']
+			'parent_id' => ['required'],
 		];
-    }
-
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
     }
 }
